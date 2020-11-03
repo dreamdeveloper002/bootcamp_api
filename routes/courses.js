50fb117d151a9f9875211966e9d940e493cd2beb
dreamdeveloper002 <bkosemani1@gmail.com>
2020-11-03 23:46:34 +0100

    advancedResults middleware

--- routes/courses.js
+++ routes/courses.js
@@ -2,10 +2,16 @@ const express = require('express');
 const {getCourses, getCourse, addCourse, updateCourse, deleteCourse}   = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
+const Courses = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults')
+
 
 router
 .route('/')
-.get(getCourses)
+.get(advancedResults(Courses, {
+  path: 'bootcamp',
+  select: 'name description'
+}), getCourses)
 .post(addCourse);
 
 
